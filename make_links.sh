3c75bff30b4256bec06b241bf006bd6861324e4e
FS Programmer <prog@fs10pc.mpifr-bonn.mpg.de>
2022-09-07 15:15:14 +0000

    add and remove various files from repo, that might be required or not

---
+++ make_links.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+ln -s /usr2/st/antcn/VLBIsend.py bin/
+ln -s /usr2/st/autoftp/autoftp bin/
+ln -s /usr2/st/jbscr/lgput_jv bin/lgput
+ln -s /usr2/st/mk5logd/mk5logd.py bin/
--- rxvt_2005/debug.h
+++
@@ -1,3 +0,0 @@
-void *safemalloc(int, char *identifier);
-void *safefree(void *ptr, char *which1, char *which2);
-void check_all_mem(char *which1, char *which2);
